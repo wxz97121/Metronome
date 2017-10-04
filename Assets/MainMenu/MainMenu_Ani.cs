7ffedc5bdd66813d78aae72e831b70e06c52c95b
wxz97121 <wxz97121@gmail.com>
2017-10-04 16:40:14 +0800

    Update to 2017.1.1

--- Assets/MainMenu/MainMenu_Ani.cs
+++ Assets/MainMenu/MainMenu_Ani.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainMenu_Ani : MonoBehaviour {
+public class MainMenu_Ani : MonoBehaviour
+{
     public Sprite[] AniSprite;
     private float LastTime = -1;
     private int nowSprite = 0;
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         float nowTime = Time.time;
         if (nowTime > LastTime + 0.25f)
         {
