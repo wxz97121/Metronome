1810a1c5ef0eace78bfab7b44061bd5cb4074636
qyj <qyjbeijing@163.com>
2017-11-17 17:35:51 +0800

    测试版本：增加了弹簧、冰面手感变化的代码。 需要更改：最大速度保护。

---
+++ Assets/Demo2/Spring.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour {
+
+	public int playerLayer;
+	public Vector2 springSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if(other.gameObject.layer == playerLayer)
+		{
+
+		
+			if (other.GetComponent<Rigidbody2D>())
+			{
+				nowFly(other.GetComponent<Rigidbody2D>());
+			}
+			else
+			{
+				print("can't found Rigidbody2D");
+			}
+
+
+		}
+
+
+
+	}
+
+
+	private void nowFly(Rigidbody2D player)
+	{
+
+		player.velocity = springSpeed;
+
+	}
+}
---
+++ Assets/Demo2/iceplayerForce.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iceplayerForce : MonoBehaviour {
+
+	public int playerLayer;
+
+	public float iceMoveForce;
+
+	public float iceMaxSpeed;
+
+	public GameMode2 gameMode2;
+
+
+
+
+	[SerializeField]
+	private float MoveForce;
+	[SerializeField]
+	private float MaxSpeed;
+
+
+	private void Awake()
+	{
+		if(gameMode2!= null)
+		{
+			MoveForce = gameMode2.moveforce;
+			MaxSpeed = gameMode2.maxspeed;
+		}
+		else
+		{
+			print("gameMode2 is Null");
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		
+	}
+
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		
+		if(collision.gameObject.layer == playerLayer)
+		{
+
+			if (collision.gameObject.GetComponent<Character2D>())
+			{
+				Character2D playerNow = collision.gameObject.GetComponent<Character2D>();
+
+				playerNow.MoveForce = iceMoveForce;
+				playerNow.m_MaxSpeed = iceMaxSpeed;
+
+			}
+			else
+			{
+				print("找不到Character2S");
+			}
+
+
+		}
+
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+
+		if (collision.gameObject.layer == playerLayer)
+		{
+
+			if (collision.gameObject.GetComponent<Character2D>())
+			{
+				Character2D playerNow = collision.gameObject.GetComponent<Character2D>();
+				
+
+				playerNow.MoveForce = MoveForce;
+				playerNow.m_MaxSpeed = MaxSpeed;
+			}
+			else
+			{
+				print("找不到Character2S");
+			}
+
+
+		}
+
+
+	}
+}
