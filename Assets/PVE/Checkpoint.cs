a74a573a8d9ccd1013aa502898de4a2a2ec6c375
软泥怪的大裤衩 <949023223@qq.com>
2017-11-24 15:30:57 +0800

    PVE && Pic

---
+++ Assets/PVE/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag != "Player") return;
+		GameObject.FindGameObjectWithTag ("GameController").GetComponent<PVE_GameMode>().Respawn_Pos = transform.position;
+		Destroy (gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/PVE/PVE_GameMode.cs
+++ Assets/PVE/PVE_GameMode.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PVE_GameMode : GameMode_base {
     public Vector3 Respawn_Pos;
+	public Character2D m_Char;
     public override Vector3 RespawnLocation()
     {
         return Respawn_Pos;
@@ -16,6 +17,7 @@ public class PVE_GameMode : GameMode_base {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (m_Char.HP == 0)
+			m_Char.StartCoroutine (m_Char.Respawn());
 	}
 }
