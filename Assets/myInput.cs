521f98bfac50892a4360e30594219ebe8ea74d97
wxz97121 <wxz97121@gmail.com>
2017-12-15 15:28:33 +0800

    加入手柄适配，修复BUG

--- Assets/myInput.cs
+++ Assets/myInput.cs
@@ -2,21 +2,25 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class myInput : MonoBehaviour {
+public class myInput : MonoBehaviour
+{
     public KeyCode moveLeft;
     public KeyCode moveRight;
+    public string JoyStickMove;
     public KeyCode Jump;
-    public KeyCode Stop;
-    public KeyCode RushLeft;
-	public KeyCode RushRight;
-    public KeyCode HardAttack;
+    public KeyCode JoyStickJump;
+    //public KeyCode Stop;
+    //public KeyCode RushLeft;
+    //public KeyCode RushRight;
+    //public KeyCode HardAttack;
+    
     //public int Type;
     // Use this for initialization
     private Character2D m_Character;
 
-	[SerializeField]
-	private bool m_Jump;
-    private int direction = 1;
+    [SerializeField]
+    private bool m_Jump;
+    private float direction = 1;
     private void Awake()
     {
         m_Character = GetComponent<Character2D>();
@@ -29,7 +33,7 @@ public class myInput : MonoBehaviour {
         {
             // Read the jump input in Update so button presses aren't missed.
             if (m_Character.isFly) m_Jump = Input.GetKey(Jump);
-            else m_Jump = Input.GetKeyDown(Jump);
+            else m_Jump = Input.GetKeyDown(Jump) | Input.GetKeyDown(JoyStickJump);
         }
     }
 
@@ -37,15 +41,16 @@ public class myInput : MonoBehaviour {
     private void FixedUpdate()
     {
         direction = 0;
+        direction=Input.GetAxis(JoyStickMove);
         if (Input.GetKey(moveLeft)) direction = -1;
         if (Input.GetKey(moveRight)) direction = 1;
-		
+        
         // Pass all parameters to the character control script.
         m_Character.Move(direction, m_Jump);
         //if (Input.GetKey(Stop)) m_Character.StartCoroutine(m_Character.Stop());
-        if (Input.GetKey(RushLeft)) m_Character.StartCoroutine(m_Character.Rush(-1));
-		if (Input.GetKey(RushRight)) m_Character.StartCoroutine(m_Character.Rush(1));
-		//if (Input.GetKey(HardAttack)) m_Character.StartCoroutine(m_Character.Hard_Attack());
+        //if (Input.GetKey(RushLeft)) m_Character.StartCoroutine(m_Character.Rush(-1));
+        //if (Input.GetKey(RushRight)) m_Character.StartCoroutine(m_Character.Rush(1));
+        //if (Input.GetKey(HardAttack)) m_Character.StartCoroutine(m_Character.Hard_Attack());
         m_Jump = false;
     }
 }
