436c1fc7906ff103a93e03d74f11f5e13462c4e6
LAPTOP-7V28TAK7\qyjbe <1798174328@163.com>
2017-12-15 17:10:14 +0800

    水下关

---
+++ Assets/WaterDemo/Divingde.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Divingde : MonoBehaviour
+{
+
+    public float Duration = 5;
+    private bool used = false;
+    // Use this for initialization
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (used) return;
+        if (other.gameObject.tag == "Player")
+        {
+            used = true;
+            other.GetComponent<SpriteRenderer>().color = Color.blue;
+            other.GetComponent<GoingDown>().isActive = true;
+            Destroy(GetComponent<SpriteRenderer>());
+            StartCoroutine(reset(other.GetComponent<Character2D>()));
+        }
+    }
+    // Update is called once per frame
+    public IEnumerator reset(Character2D m_Player)
+    {
+        yield return new WaitForSeconds(Duration);
+        m_Player.GetComponent<SpriteRenderer>().color = Color.white;
+        m_Player.GetComponent<GoingDown>().isActive = false;
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/WaterDemo/GoingDown.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoingDown : MonoBehaviour {
+
+
+    public KeyCode goingDownKey;
+
+    public float forceOfGoingDown;
+
+
+    Rigidbody2D rigdidbodyOfMy;
+
+
+    bool InWater;
+    public bool isActive = false;
+
+
+    private void Awake()
+    {
+
+        if (!(rigdidbodyOfMy = GetComponent<Rigidbody2D>())) print(1);
+        isActive = false;
+        InWater = false;
+
+
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+		
+	}
+
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(goingDownKey) && InWater && isActive)
+        {
+
+
+            rigdidbodyOfMy.AddForce(new Vector2(0, -forceOfGoingDown));
+
+
+        }
+    }
+
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Water")
+        {
+            InWater = true;
+        }
+
+
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Water")
+        {
+            InWater = false;
+        }
+
+    }
+
+}
