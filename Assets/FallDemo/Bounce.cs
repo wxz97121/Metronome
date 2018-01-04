f76781f7e6cfce849ca7e8648e85548e50b4a97b
LAPTOP-7V28TAK7\qyjbe <1798174328@163.com>
2018-01-04 17:30:02 +0800

    增加了4p下落关卡。

---
+++ Assets/FallDemo/Bounce.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+    Rigidbody2D rigidbodyPlayer;
+
+    public Vector2 velocityOfBounce;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.GetComponent<Rigidbody2D>())
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().velocity += velocityOfBounce;
+            }
+            else
+            {
+                print("Can't Found Rigidbody");
+            }
+        }
+    }
+}
--- Assets/FallDemo/PlatformRefresher.cs
+++ Assets/FallDemo/PlatformRefresher.cs
@@ -9,13 +9,19 @@ public class PlatformRefresher : MonoBehaviour
     public float MinRefreshTime = 2;
     public float MaxRefreshTime = 3;
     public GameObject PlatformPrefab;
-    public double Uptime;
+    public float minUptime;
+    public float maxUptime;
     private float Lastx = 100000;
     [HideInInspector]
     public GameObject LastPlat;
     public float DistLimit;
-    public float RespawnHeight=550;
+    public float RespawnHeight = 550;
     // Use this for initialization
+    public float BoardLen;
+    public float POfShake = 0.08f;
+    public float minXSpeed = 2000;
+    public float maxXSpeed = 2250;
+    public GameObject LRPlatformPrefab;
     void Start()
     {
         StartCoroutine(Refresh());
@@ -25,19 +31,41 @@ public class PlatformRefresher : MonoBehaviour
     {
         while (true)
         {
+            //for(int i=0; i < 2; i++)
 
             float Newx = Random.Range(LeftPos.position.x, RightPos.position.x);
             while (Mathf.Abs(Newx - Lastx) < DistLimit)
             {
                 Newx = Random.Range(LeftPos.position.x, RightPos.position.x);
             }
+            //if (i == 1)
+            //{
+            //    while (Mathf.Abs(Newx-positionX1) < (378*2))
+            //    {
+            //        Newx = Random.Range(LeftPos.position.x, RightPos.position.x);
+            //    }
+            //}
+
             Lastx = Newx;
             GameObject newPlat = Instantiate(PlatformPrefab, new Vector3(Lastx, LeftPos.position.y, 100), Quaternion.identity);
             var m_Comp = newPlat.AddComponent<RisingPlatform>();
             m_Comp.BeginPos = newPlat.transform.position;
             m_Comp.TargetPos = newPlat.transform.position + new Vector3(0, 2700, 0);
-            m_Comp.MoveTime = Uptime;
+            //if (Random.value < POfShake) m_Comp.TargetPos += new Vector3((Random.value > 0.5 ? 1 : -1) * Random.Range(minXSpeed, maxXSpeed), 0, 0);
+            m_Comp.MoveTime = Random.Range(minUptime, maxUptime);
             LastPlat = newPlat;
+            //Random.InitState(((int)LastPlat.transform.position.x));
+
+            //if (Random.value < POfShake) //有概率从左右刷出
+            //{
+            //    GameObject newLRPlat = Instantiate(LRPlatformPrefab, new Vector3(2316, -114, 0), Quaternion.identity);
+            //    m_Comp = newLRPlat.AddComponent<RisingPlatform>();
+            //    m_Comp.BeginPos = newLRPlat.transform.position;
+            //    m_Comp.TargetPos = newLRPlat.transform.position - new Vector3(5000, 0, 0);
+            //    //if (Random.value < POfShake) m_Comp.TargetPos += new Vector3((Random.value > 0.5 ? 1 : -1) * Random.Range(minXSpeed, maxXSpeed), 0, 0);
+            //    m_Comp.MoveTime = Random.Range(minUptime, maxUptime);
+            //}
+
             yield return new WaitForSeconds(Random.Range(MinRefreshTime, MaxRefreshTime));
             //Refreshindex = (Refreshindex + 1) % PlatformPrefab.Length;
         }
@@ -45,6 +73,6 @@ public class PlatformRefresher : MonoBehaviour
     [SerializeField]
     public void FallRefresh()
     {
-        GetComponent<GameMode_base>().RespawnLocation=LastPlat.transform.position + new Vector3(0, RespawnHeight, 0);
+        GetComponent<GameMode_base>().RespawnLocation = LastPlat.transform.position + new Vector3(Random.Range(-BoardLen / 2, BoardLen / 2), RespawnHeight, 0);
     }
 }
